d527d1567e43af23ed48201f597acd19e41eee39
jobarrere <jordy.barrere@etu.uca.fr>
2026-03-10 08:29:46 +0100

    adding a generic input composant
    ## Détails
    - Composant générique réutilisable au sein de toute l'application.
    -
    -
    
    ## Impacts
    - [ ] Aucun
    - [ ] Backend
    - [ ] Base de données
    - [X] UI/UX
    - [ ] Breaking change
    
    ## Issue liée
    #11
    
    Refs: #%{iid}

--- core/app/src/main/java/app/jardinageons/MainActivity.kt
+++ core/app/src/main/java/app/jardinageons/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import app.jardinageons.presentation.components.AppNavGraph
+import androidx.compose.material3.TextField
 import app.jardinageons.presentation.features.seedInventory.SeedInventoryScreen
 import app.jardinageons.presentation.theme.JardinageonsTheme
 
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        
+
         setContent {
             JardinageonsTheme {
                 AppNavGraph()
