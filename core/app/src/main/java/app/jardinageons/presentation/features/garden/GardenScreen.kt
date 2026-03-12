b4d5ccb92a4870d0f12f1b85fe29d315441f60ef
etveau <etienne.veau@etu.uca.fr>
2026-03-12 11:43:24 +0100

    correction unresolved reference

--- core/app/src/main/java/app/jardinageons/presentation/features/garden/GardenScreen.kt
+++ core/app/src/main/java/app/jardinageons/presentation/features/garden/GardenScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -128,7 +127,8 @@ fun GardenScreen(
 
             Box(
                 modifier = Modifier
-                    .matchParentSize()
+                    .fillMaxWidth()
+                    .height(56.dp)
                     .clickable(
                         interactionSource = interactionSource,
                         indication = null
